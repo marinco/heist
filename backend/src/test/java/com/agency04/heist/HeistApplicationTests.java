adc4229af037f0e84009c64ffafd4532a8a2d606
Marin Milina <marin.milina@5x9networks.com>
2021-06-02 11:42:20 +0200

    Init backend project

---
+++ backend/src/test/java/com/agency04/heist/HeistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency04.heist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
